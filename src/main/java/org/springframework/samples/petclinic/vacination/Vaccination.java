a58addfd78cf6bbdcac8210c853790401063c937
raugalroc <raugalroc@alum.us.es>
2022-01-14 14:28:40 +0100

    Examen lab

--- src/main/java/org/springframework/samples/petclinic/vacination/Vaccination.java
+++ src/main/java/org/springframework/samples/petclinic/vacination/Vaccination.java
@@ -2,19 +2,48 @@ package org.springframework.samples.petclinic.vacination;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.persistence.Transient;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.persistence.Id;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
 
 import org.springframework.samples.petclinic.pet.Pet;
+
 
 import lombok.Getter;
 import lombok.Setter;
 
+
 @Getter
 @Setter
+@Entity
+
 public class Vaccination {
-    Integer id;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	protected Integer id;
+	
+	@NotNull
+	@DateTimeFormat(pattern = "yyyy/MM/dd")
     LocalDate date;
+	
+	@ManyToOne
+	@NotNull
+	@JoinColumn(name = "vaccinated_pet_id")
     Pet vaccinatedPet;   
+    
+	@ManyToOne
     @Transient
     Vaccine vaccine; 
 }
